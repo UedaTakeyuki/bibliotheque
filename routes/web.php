9793b863bb0d8373fe5efbee16feb4febe99ba13
Takeyuki UEDA <ueda@latelierdueda.com>
2017-12-06 19:34:32 +0900

    login require

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
+Route::group(['middleware' => ['auth']], function () {
+  // refer https://qiita.com/washio12/items/59f5cde23b4205973c6b
+	Route::get('/', function () {
     return view('welcome');
-});
-Route::resource('bibliographies','BibliographyController');
+	});
+	Route::resource('bibliographies','BibliographyController');
 
+});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
