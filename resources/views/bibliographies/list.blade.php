a66329f983af0d549c8b71de78035151cc0d339c
Takeyuki UEDA <ueda@latelierdueda.com>
2017-12-11 20:02:56 +0900

    l&f

--- resources/views/bibliographies/list.blade.php
+++ resources/views/bibliographies/list.blade.php
@@ -15,12 +15,12 @@
         	<form method="post" action="/bibliographies/{{$bibliography->id}}">
                 <input name="_method" type="hidden" value="delete">
                 <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <input type="submit" value="削除" class="btn btn-danger btn-sm btn-destroy" onclick="javascript:return confirm('本当に削除しますか？')">
+                <input type="submit" value="削除" class="btn btn-sm btn-destroy" onclick="javascript:return confirm('本当に削除しますか？')">
           </form>
           <b>登録日: </b>{{$bibliography->created_at}}<br>
-          <b>タイトル: </b>{{$bibliography->title}}<br>;
-          <b>価格: </b>{{$bibliography->price}}<br>;
-          <b>メモ: </b>{{$bibliography->memo}}<br><hr>;
+          <b>タイトル: </b>{{$bibliography->title}}<br>
+          <b>価格: </b>{{$bibliography->price}}<br>
+          <b>メモ: </b>{{$bibliography->memo}}<br><hr>
         </article>
     @endforeach
 @endsection
